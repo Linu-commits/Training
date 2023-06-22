c8003d4b0a158cde73bc590a99e1fb4ea591f305
Linu-commits <137082882+Linu-commits@users.noreply.github.com>
2023-06-22 20:26:02 +0530

    Add files via upload
    first commit

---
+++ AddInteger.java
@@ -0,0 +1,22 @@
+class AddInteger{
+
+public static void main(String args[]){
+	
+//add two integer program	
+int a =10;
+int b =20;
+int c= a+b;
+System.out.println(c);
+
+//add one float and integer
+
+int   d =10;
+float e =0.5f;
+float f = d+e;
+System.out.println(f);
+
+//print Name and Age
+
+System.out.println("Name:Linu" + "\n" + "Age:28");
+}
+}
